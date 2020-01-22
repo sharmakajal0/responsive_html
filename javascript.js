e19585937e1f60516ffff0ca1d5ab288cd0fa680
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-22 20:03:56 +0530

    added some changes to index.html file

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function sayHello(){
-    console.log("Say Hello");
-    $('#text').html("Goodbye");
-    $("#text").click(function(){
-        $('#text').html("Hello");
-        $("#text").off("click");  
-    });
-};
